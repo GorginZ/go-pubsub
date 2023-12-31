1c90944293f36f050d461f3ebd1ed43763112e26
georgia leng <58770450+GorginZ@users.noreply.github.com>
2023-12-31 12:39:10 +1100

    use aus, make bucket easy to destroy. may not actualy use remote state

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,8 +4,8 @@ resource "random_id" "bucket_prefix" {
 
 resource "google_storage_bucket" "default" {
   name          = "${random_id.bucket_prefix.hex}-bucket-tfstate"
-  force_destroy = false
-  location      = "asia"
+  force_destroy = true # this is just a demo want it to be easy to clean up
+  location      = "AUSTRALIA-SOUTHEAST2"
   storage_class = "STANDARD"
   versioning {
     enabled = true

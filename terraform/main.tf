6696fd96dd767b78a802786e77c2d239bd729ded
georgia leng <58770450+GorginZ@users.noreply.github.com>
2023-12-31 13:09:28 +1100

    mv project resource to main where bucket is otherwise hard to read

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,20 @@
+locals {
+  services = ["pubsub.googleapis.com"]
+}
+
+resource "google_project" "go_pubsub" {
+  name       = "go-pubsub"
+  project_id = "go-pubsub"
+}
+
+# enable apis
+resource "google_project_service" "go_pubsub_services" {
+  count                      = length(local.services)
+  project                    = google_project.go_pubsub.project_id
+  service                    = local.services[count.index]
+  disable_dependent_services = true
+}
+
 resource "random_id" "bucket_prefix" {
   byte_length = 8
 }

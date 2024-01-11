d73591bf2481ded4405311eb8356917c524ebfac
georgia leng <58770450+GorginZ@users.noreply.github.com>
2024-01-11 16:18:46 +1100

    wip dump for liam to play with

--- go-pubsub/order/order.go
+++ go-pubsub/order/order.go
@@ -1,13 +1,107 @@
 package order
 
 import (
+	"fmt"
+	"log"
+	"math/rand"
+
+	"context"
+	"os"
+
 	"cloud.google.com/go/pubsub"
+	"github.com/gin-gonic/gin"
 )
 
-func publish(client *pubsub.Client, topic *pubsub.Topic, message string) error {
-	result := topic.Publish(client.Context(), &pubsub.Message{
-		Data: []byte(message),
+// Order represents an order from the frontend
+type Order struct {
+	Email   string `json:"email"`
+	Product string `json:"product"`
+	Amount  int    `json:"amount"`
+}
+
+type OrderCreated struct {
+	Order Order  `json:"order"`
+	Id    string `json:"id"`
+}
+
+func generateOrderID() string {
+	id := rand.Intn(99999)
+	return fmt.Sprintf("%05d", id)
+}
+
+func publishOrderCreated(client *pubsub.Client, order OrderCreated) error {
+	ctx := context.Background()
+	topicID := os.Getenv("TOPIC_ID")
+	topic := client.Topic(topicID)
+
+	// publish order created event
+	result := topic.Publish(ctx, &pubsub.Message{
+		Data: []byte("order created"),
 	})
-	_, err := result.Get(client.Context())
-	return err
+
+	// block until publish is finished
+	_, err := result.Get(ctx)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// handle order will generate a order id and create a new order in an inmemory database and publish the order to pubsub
+func handleOrder(c *gin.Context, client *pubsub.Client) {
+	var order Order
+	if err := c.BindJSON(&order); err != nil {
+		c.JSON(400, gin.H{"message": "invalid request"})
+		return
+	}
+	// generate order id
+	id := generateOrderID()
+
+	o := OrderCreated{
+		Order: order,
+		Id:    id,
+	}
+
+	// todo create order in inmem db doesn't matter
+
+	// publish order created event
+	err := publishOrderCreated(client, o)
+
+	if err != nil {
+		c.JSON(500, gin.H{"message": "internal server error"})
+		return
+	}
+
+	c.JSON(200, gin.H{"message": "order created"})
+}
+
+func createAndConfigureClient() (*pubsub.Client, error) {
+	// get envs
+	projectID := os.Getenv("PROJECT_ID")
+	topicID := os.Getenv("TOPIC_ID")
+
+	if projectID == "" || topicID == "" {
+		return nil, fmt.Errorf("PROJECT_ID and TOPIC_ID must be set")
+	}
+
+	// create client
+	ctx := context.Background()
+	client, err := pubsub.NewClient(ctx, projectID)
+	client.Topic(topicID)
+
+	return client, err
+}
+
+func main() {
+	c, err := createAndConfigureClient()
+	if err != nil {
+		// todo better exit
+		log.Fatal(err)
+	}
+
+	gin.SetMode(gin.ReleaseMode)
+	r := gin.Default()
+	r.POST("/order", handleOrder(), c)
+	log.Fatal(r.Run(":8080"))
 }

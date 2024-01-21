bb4e26131894b59273410be29c758a421c58d890
Georgia Leng <georgia.z.leng@gmail.com>
2024-01-21 15:53:53 +1100

    rm the execdir nonsenes, just cd to the module and do go test./... so we get an easier exit 1 on failure...

--- scripts/go-test.sh
+++ scripts/go-test.sh
@@ -17,16 +17,13 @@ die() { echo "$1" >&2; exit "${2:-1}"; }
 hash docker || die "docker not found" $?
 
 test_action=$1
-
 case $test_action in
     order|payment|shipping)
-        # run test for specific service only no need to find
-        # docker compose run --rm go-sh cd go-pubsub/$test_action && go test ./...  ## todo fix this difficulties with the compose service entrypoint for the alpine go img.
+        docker compose run --rm go-sh sh -c "cd go-pubsub/$test_action && go test ./...";
         ;;
-
+#example for running all tests for the exercise, we'll build these services out too
     all)
-        # docker compose run --rm go-sh find . -name go.mod -execdir go test ./... \; #this cmd works locally but alpine uses busybox find and doesnt have execdir...
-        docker compose run --rm go-sh find . -name go.mod -exec sh -c 'cd $(dirname {}); go test ./...' \; | grep -i fail && die "tests failed" 1 # we have to exit here because the way we do all tests it exits 0 and so does grep on match. bad, I know
+        docker compose run --rm go-sh sh -c "cd go-pubsub/order && go test ./... && cd ../payment && go test ./... && cd ../shipping && go test ./..."
         ;;
 
     *)

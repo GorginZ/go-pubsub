214ac551a54af6645a1db52ad378deef253224ed
Georgia Leng <georgia.z.leng@gmail.com>
2024-01-27 17:25:30 +1100

    green tests with this complex mock thing

--- go-pubsub/order/main_test.go
+++ go-pubsub/order/main_test.go
@@ -7,58 +7,77 @@ import (
 	"strings"
 	"testing"
 
-	pubsub "cloud.google.com/go/pubsub"
+	"cloud.google.com/go/pubsub"
+
+	"cloud.google.com/go/pubsub/apiv1/pubsubpb"
 	"cloud.google.com/go/pubsub/pstest"
+
 	"github.com/gin-gonic/gin"
 	"google.golang.org/api/option"
 	"google.golang.org/grpc"
 )
 
-func Test_handOrder(t *testing.T) {
-	tests := map[string]struct {
-		wantCode int
-		request  *http.Request
-		w        *httptest.ResponseRecorder
-		context  *gin.Context
-	}{
-		"valid request": {
-			wantCode: 200,
-			request:  httptest.NewRequest("POST", "/order", strings.NewReader(`{"email": "email.com", "product": "car", "amount": 99}`)),
-			w:        httptest.NewRecorder(),
-			context:  &gin.Context{},
-		},
-		"invalid request: missing email": {
-			wantCode: 400,
-			request:  httptest.NewRequest("POST", "/order", strings.NewReader(`{"product": "car", "amount": 99}`)),
-			w:        httptest.NewRecorder(),
-			context:  &gin.Context{},
-		},
-	}
-	for name, tc := range tests {
-		t.Run(name, func(t *testing.T) {
-			ctx := GetTestContext(tc.w, tc.request)
-			client, _ := pubsub.NewClient(ctx, "go-pubsub")
-			handleOrder(ctx, client)
-			if tc.w.Code != tc.wantCode {
-				t.Fatalf("got %v, want %v", tc.w.Code, tc.wantCode)
-			}
+type HappyReactor struct{}
 
-		})
-	}
+func (r *HappyReactor) React(_ interface{}) (handled bool, ret interface{}, err error) {
+	pbr := &pubsubpb.PublishResponse{MessageIds: []string{"61"}}
+
+	return true, pbr, nil
 }
 
-func TestWithMock(t *testing.T) {
+func TestSadWithMock(t *testing.T) {
 
 	ctx := GetTestContext(httptest.NewRecorder(), httptest.NewRequest("POST", "/order", strings.NewReader(`{"email": "email.com", "product": "car", "amount": 99}`)))
+
 	// Start a fake server running locally.
-	srv := pstest.NewServer()
+	opts := []pstest.ServerReactorOption{
+		pstest.WithErrorInjection("Publish", 200, "georgia error"),
+	}
+	srv := pstest.NewServer(opts...)
 	defer srv.Close()
+
 	// Connect to the server without using TLS.
 	conn, err := grpc.Dial(srv.Addr, grpc.WithInsecure())
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer conn.Close()
+
+	// Use the connection when creating a pubsub client.
+	client, err := pubsub.NewClient(ctx, "project", option.WithGRPCConn(conn))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Close()
+	handleOrder(ctx, client)
+	if ctx.Writer.Status() != 500 {
+		t.Fatalf("got %v, want %v", ctx.Writer.Status(), 200)
+	}
+}
+
+func TestHappyWithMock(t *testing.T) {
+
+	ctx := GetTestContext(httptest.NewRecorder(), httptest.NewRequest("POST", "/order", strings.NewReader(`{"email": "email.com", "product": "car", "amount": 99}`)))
+
+	opt := pstest.ServerReactorOption{
+		FuncName: "Publish",
+		Reactor:  &HappyReactor{},
+	}
+	srv := pstest.NewServer(opt)
+	defer srv.Close()
+
+	// Connect to the server without using TLS.
+	conn, err := grpc.Dial(srv.Addr, grpc.WithInsecure())
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	//set response
+	srv.SetAutoPublishResponse(false)
+	pbr := &pubsubpb.PublishResponse{MessageIds: []string{"1"}}
+	srv.AddPublishResponse(pbr, nil)
+
 	// Use the connection when creating a pubsub client.
 	client, err := pubsub.NewClient(ctx, "project", option.WithGRPCConn(conn))
 	if err != nil {
@@ -68,6 +87,76 @@ func TestWithMock(t *testing.T) {
 	handleOrder(ctx, client)
 	if ctx.Writer.Status() != 200 {
 		t.Fatalf("got %v, want %v", ctx.Writer.Status(), 200)
+	}
+}
+
+func Test_handleOrder(t *testing.T) {
+	tests := map[string]struct {
+		wantCode int
+		request  *http.Request
+		w        *httptest.ResponseRecorder
+		context  *gin.Context
+		reactor  pstest.Reactor
+	}{
+		"valid request": {
+			wantCode: 200,
+			request:  httptest.NewRequest("POST", "/order", strings.NewReader(`{"email": "email.com", "product": "car", "amount": 99}`)),
+			w:        httptest.NewRecorder(),
+			context:  &gin.Context{},
+			reactor:  &HappyReactor{}, //will return a msg ID
+		},
+		"invalid request: missing email": {
+			wantCode: 400,
+			request:  httptest.NewRequest("POST", "/order", strings.NewReader(`{"product": "car", "amount": 99}`)),
+			w:        httptest.NewRecorder(),
+			context:  &gin.Context{},
+			reactor:  &HappyReactor{}, //not really relevant this will "fail fast"
+		},
+		"pubsub error": {
+			wantCode: 500,
+			request:  httptest.NewRequest("POST", "/order", strings.NewReader(`{"email": "email.com", "product": "car", "amount": 99}`)),
+			w:        httptest.NewRecorder(),
+			context:  &gin.Context{},
+			reactor:  pstest.WithErrorInjection("Publish", 200, "georgia error").Reactor, //will give an error
+		},
+	}
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			ctx := GetTestContext(tc.w, tc.request)
+
+			//set up server
+			opt := pstest.ServerReactorOption{
+				FuncName: "Publish",
+				Reactor:  tc.reactor,
+			}
+			srv := pstest.NewServer(opt)
+			// srv := pstest.NewServer()
+			defer srv.Close()
+			// Connect to the server without using TLS.
+			conn, err := grpc.Dial(srv.Addr, grpc.WithInsecure())
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer conn.Close()
+
+			//try set response
+			// srv.SetAutoPublishResponse(false)
+			// pbr := &pubsubpb.PublishResponse{MessageIds: []string{"1"}}
+			// srv.AddPublishResponse(pbr, nil)
+
+			// Use the connection when creating a pubsub client.
+			client, err := pubsub.NewClient(ctx, "project", option.WithGRPCConn(conn))
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer client.Close()
+
+			handleOrder(ctx, client)
+			if tc.w.Code != tc.wantCode {
+				t.Fatalf("got %v, want %v", tc.w.Code, tc.wantCode)
+			}
+
+		})
 	}
 }
 
